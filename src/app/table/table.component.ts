8267b4c8b6e87e464b85700c7498b28a43accd22
swagata1997 <63637761+swagata1997@users.noreply.github.com>
2020-04-23 11:23:28 -0600

    4th

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,38 +1,95 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
+import { MatPaginator, MatTableDataSource, MatSort, Sort, } from '@angular/material';
 import { SharedService } from '../shared.service';
-import { element } from 'protractor';
+
 @Component({
   selector: 'app-table-row',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
 
-export class TableComponent implements OnInit {
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+export class TableComponent implements OnInit, AfterViewInit {
+
+
+  @ViewChild('paginatorPos', { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
   @Input() ELEMENT_DATA: any[];
   @Input() FieldData: any;
+  objectID: any;
+  objDatas: any[] = [];
+  @Input() pointData: any;
+  SelectedRow: any;
   dataSource = new MatTableDataSource<any>();
 
+
   constructor(public sharedService: SharedService) { }
+  cellClicked(row) {
+    console.log(row);
+    let val = 'OBJECTID'
+    this.sharedService.objectID.next(row.OBJECTID);
+    this.SelectedRow = row[val];
+
+  }
+  isSorting(columnText: string): boolean {
+    console.log(columnText);
+    return true;
+  }
+
+  pagedata() {
+
+    this.paginator.pageSize = 5;
+    const skip = this.paginator.pageSize * this.paginator.pageIndex;
+    const paged = this.FieldData.filter((u, i) => i >= skip)
+      .filter((u, i) => i < this.paginator.pageSize);
+    this.objDatas.push(paged);
+    this.sharedService.objData.next(paged);
+    //console.log(this.objDatas);
+
+  }
 
   ngOnInit() {
+
+    this.SelectedRow = this.sharedService.SelectedRow;
     this.sharedService.ELEMENT_DATA.subscribe(elementData => {
       this.ELEMENT_DATA = elementData;
     });
+
+    this.sharedService.pointData.subscribe(filterData => {
+      this.pointData = filterData;
+    });
+
     this.sharedService.FieldData.subscribe(fieldData => {
       this.FieldData = JSON.parse(JSON.stringify(fieldData));
       this.dataSource = new MatTableDataSource(this.FieldData);
       this.dataSource.paginator = this.paginator;
     });
+
+
+
+
     this.FieldData = JSON.parse(JSON.stringify(this.FieldData));
+
+
+
+
+    /***************Map Data************* */
     this.dataSource = new MatTableDataSource(this.FieldData);
+
+    setTimeout(() => {
+      this.paginator.pageIndex = 0;
+      this.paginator.pageSize = 5;
+      this.dataSource.paginator = this.paginator;
+    });
+
+    this.pagedata();
+
+
 
   }
 
   ngAfterViewInit(): void {
-
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
 }
