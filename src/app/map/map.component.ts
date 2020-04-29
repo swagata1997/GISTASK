fd695b15ee23e4753b9272936009b3b4c68a9ab6
swagata1997 <63637761+swagata1997@users.noreply.github.com>
2020-04-29 18:50:29 +0530

    fifth

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, Input, OnDestroy, ViewChild, ElementRef, Query } from '@angular/core';
-import { loadModules } from "esri-loader";
+import { loadModules } from 'esri-loader';
 import { SharedService } from '../shared.service';
 import { MatPaginator, MatTableDataSource } from '@angular/material';
 import { FormGroup, FormControl } from '@angular/forms';
-//import { DataSource } from '@angular/cdk/table';
 
 export interface PeriodicElement {
   name: string;
@@ -11,38 +10,37 @@ export interface PeriodicElement {
   weight: number;
   symbol: string;
 }
-
-let ELEMENT_DATA: any[] = [];
-let FieldData: any[] = [];
-let geometry: any[] = [];
-
+const ELEMENT_DATA: any[] = [];
+const FieldData: any[] = [];
+const graphicArray: any[] = [];
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit, OnDestroy {
-  @ViewChild("mapViewNode", { static: true }) private mapViewEl: ElementRef;
+  @ViewChild('mapViewNode', { static: true }) private mapViewEl: ElementRef;
   dataSource: MatTableDataSource<Element>;
-
-  view: any;
+ view: any;
+  // tslint:disable-next-line:variable-name
   _map: any;
   layerId: string;
+  // tslint:disable-next-line:variable-name
   _params: any;
+  // tslint:disable-next-line:variable-name
   _qTask: any;
   ELEMENT_DATA: any[] = [];
   FieldData: any[] = [];
-  geometry:any[] = [];
+  geometry: any[] = [];
   MapData: any[] = [];
+  graphicArray: any[] = [];
   pointData: any;
   FLayer: any;
   AttrData: any;
-  filterName: string
+  filterName: string;
   paginator: MatPaginator;
- //fieldName = 'x';
- // value = 'y';
-  lat:any;
-  log:any;
+  lat: any;
+  log: any;
   @Input() objectID: any[];
   @Input() objData: any[];
 
@@ -52,22 +50,22 @@ export class MapComponent implements OnInit, OnDestroy {
 
   }
 
-
   async initializeMap() {
 
     try {
       // Load the modules for the ArcGIS API for JavaScript
+      // tslint:disable-next-line:no-shadowed-variable
       const [Map, MapView, FeatureLayer, QueryTask, Query, Graphic, GraphicsLayer] =
-        await loadModules(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/tasks/QueryTask",
-          "esri/tasks/support/Query", "esri/Graphic", "esri/layers/GraphicsLayer"]);
+        await loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer', 'esri/tasks/QueryTask',
+          'esri/tasks/support/Query', 'esri/Graphic', 'esri/layers/GraphicsLayer']);
 
       // Configure the Map
       const mapProperties = {
-        basemap: "topo"
+        basemap: 'topo'
       };
 
       this._map = new Map(mapProperties);
-
+      console.log(this._map);
       // Initialize the MapView
       const mapViewProperties = {
         container: this.mapViewEl.nativeElement,
@@ -78,99 +76,95 @@ export class MapComponent implements OnInit, OnDestroy {
 
       this.view = new MapView(mapViewProperties);
       const queryTask = {
-        url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/HUD%20REO%20Properties/FeatureServer/0"
-      }
-      this._qTask = new QueryTask(queryTask)
+        url: 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/HUD%20REO%20Properties/FeatureServer/0'
+      };
+      this._qTask = new QueryTask(queryTask);
       const QueryFeature = {
         returnGeometry: true,
-        outFields: ["*"]
-      }
-      this._params = new Query(QueryFeature);
-
-      for(var i=0;i<FieldData.length;i++){
-       var lat = FieldData[i].MAP_LATITUDE;
-       var log = FieldData[i].MAP_LONGITUDE;
-      var markerSymbol = {
-        type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-        color: [226, 119, 40],
-        outline: {
-          // autocasts as new SimpleLineSymbol()
-          color: [255, 255, 255],
-          width: 2
-        }
+        outFields: ['*']
       };
-      var point = {
-        type: "point",
-        longitude:lat,
-        latitude: log
-      }
-     var graphic = new Graphic({
-        geometry: point,
-        symbol: markerSymbol
+      this._params = new Query(QueryFeature);
+      console.log(FieldData);
+      // tslint:disable-next-line:prefer-for-of
 
-     });
-    var layer = new GraphicsLayer({
-       
-      });
-      layer.add(graphic);
-    }
-      this._map.add(layer);
 
-     
-      var qTask = new QueryTask({
-        url:"https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/HUD%20REO%20Properties/FeatureServer/0"
+
+
+      const qTask = new QueryTask({
+        url: 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/HUD%20REO%20Properties/FeatureServer/0'
       });
 
-      var _params = new Query({
+      // tslint:disable-next-line:variable-name
+      const _params = new Query({
         returnGeometry: true,
-        outFields: ["*"]
+        outFields: ['*']
       });
       _params.where = '1=1';
 
       qTask.execute(_params).then(getResults)
         .catch(promiseRejected);
+      // tslint:disable-next-line:prefer-for-of
+      setTimeout(function() {
+     // tslint:disable-next-line:prefer-for-of
+     for (let i = 0; i < FieldData.length; i++) {
+          console.log(this._map);
+          const lat = FieldData[i].MAP_LATITUDE;
+          const log = FieldData[i].MAP_LONGITUDE;
+          const markerSymbol = {
+           type: 'simple-marker', // autocasts as new SimpleMarkerSymbol()
+           color: [226, 119, 40],
+           outline: {
+             // autocasts as new SimpleLineSymbol()
+             color: [255, 255, 255],
+             width: 2
+           }
+         };
+          const point = {
+          type: 'point',
+          longitude: lat,
+          latitude: log
+        };
+          const graphic = new Graphic({
+             geometry : point,
+             symbol : markerSymbol
+          });
 
-     console.log(ELEMENT_DATA);
-     console.log(FieldData);
+          graphicArray.push(graphic);
+
+       } }, 3000);
+      const layer = new GraphicsLayer({
+             graphics: [graphicArray]
+           });
+      this._map.add(layer);
+     //  console.log(layer);
 
     } catch (error) {
-      console.log("EsriLoader: ", error);
+      console.log('EsriLoader: ', error);
     }
     function getResults(response) {
-      
-      for (var i = 0; i < response.fields.length; i++) {
-        //console.log(response.fields[i].name);
-          ELEMENT_DATA.push(response.fields[i].name);
-         
-         
-      }
-      var symbol; var geometry;
+      // tslint:disable-next-line:prefer-for-of
+      for (let i = 0; i < response.fields.length; i++) {
+        ELEMENT_DATA.push(response.fields[i].name);
+         }
       response.features.forEach(value => {
-        
+
         FieldData.push(value.attributes);
-        //console.log(FieldData.length);
-       /* var lat = value.geometry.latitude;
-        var log = value.geometry.longitude;
-        if( value.geometry.type==="point"){
-          symbol=  markerSymbol;
-          geometry.push(value.geometry);
-         // return geometry;
-        }*/
-       // this._map.graphics.add(new Graphic(value.geometry, symbol));
-    })
-      this.sharedService.ELEMENT_DATA.next(this.ELEMENT_DATA);
-      this.sharedService.FieldData.next(this.FieldData);
-      //console.log(ELEMENT_DATA);
-      return { ELEMENT_DATA: ELEMENT_DATA, FieldData: FieldData,geometry };
-    }
-    /***********************END****************** */
+
+    });
+      // this.sharedService.ELEMENT_DATA.next(this.ELEMENT_DATA);
+      // this.sharedService.FieldData.next(this.FieldData);
+     // console.log(ELEMENT_DATA);
+      // console.log(FieldData);
+      return { ELEMENT_DATA, FieldData};
+      }
+   /***********************END****************** */
     function promiseRejected(error) {
-      console.error("Promise rejected: ", error.message);
+      console.error('Promise rejected: ', error.message);
 
     }
 
 
-  }//End Map
+  }// End Map
 
   /*******************Get Map Record********** */
   /**************Get all first page data *****************/
@@ -179,8 +173,9 @@ export class MapComponent implements OnInit, OnDestroy {
       // console.log(elementData);
       let query = '';
 
-      for (var i = 0; i < elementData.length; i++) {
-        query += '"' + elementData[i].OBJECTID + '",'
+      // tslint:disable-next-line:prefer-for-of
+      for (let i = 0; i < elementData.length; i++) {
+        query += '"' + elementData[i].OBJECTID + '",';
       }
 
       const arrData = query.substring(0, query.length - 1);
@@ -210,7 +205,7 @@ export class MapComponent implements OnInit, OnDestroy {
     console.error("Promise rejected: ", error.message);
 
   }*/
-  filterData() {
+ /* filterData() {
     const propertyLayer = this.view.map.layers.find((layer) => { return layer.id === 'properties' })
 
     const str = (<HTMLInputElement>document.getElementById("textValue")).value;
@@ -246,24 +241,23 @@ export class MapComponent implements OnInit, OnDestroy {
     this.initializeMap();
     this.ELEMENT_DATA = ELEMENT_DATA;
     this.FieldData = FieldData;
-    // console.log(FieldData);
-
     /**********On Table click highlight Feature Layer**** */
     this.sharedService.objectID.subscribe(elementData => {
-      let highlight;
+      // let highlight;
       this.objectID = elementData;
       const objectID = this.objectID;
-      if (this.objectID) {
-        const propertyLayer = this.view.map.layers.find((layer) => { return layer.id === 'properties' });
-        this.view.whenLayerView(propertyLayer).then(function (layerView) {
+     /* if (this.objectID) {
+        const propertyLayer = this.view.map.layers.find((layer) => layer.id === 'properties');
+        // tslint:disable-next-line:only-arrow-functions
+        this.view.whenLayerView(propertyLayer).then(function(layerView) {
           //  console.log(highlight);
           if (highlight) {
             highlight.remove();
           }
           highlight = layerView.highlight(objectID);
           // console.log(highlight);
-        })
-      }
+        });
+      } ;*/
 
     });
 
