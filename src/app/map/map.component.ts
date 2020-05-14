4d441300153bf401907f673a1278928b73767abd
swagata1997 <63637761+swagata1997@users.noreply.github.com>
2020-05-14 22:59:56 +0530

    14/05/20

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,159 +1,208 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef, Query } from '@angular/core';
-import { loadModules } from "esri-loader";
+import { Component, OnInit, Input, OnDestroy, ViewChild, ElementRef, Query } from '@angular/core';
+import { loadModules } from 'esri-loader';
 import { SharedService } from '../shared.service';
+import { MatTableDataSource } from '@angular/material';
+
+
 export interface PeriodicElement {
   name: string;
   position: number;
   weight: number;
   symbol: string;
 }
+const ELEMENT_DATA: any[] = [];
+const FieldData: any[] = [];
+const graphicArray: any[] = [];
 
-let ELEMENT_DATA: any[] = [];
-let FieldData: any[] = [];
-let featureLayerView: any;
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit, OnDestroy {
-  @ViewChild("mapViewNode", { static: true }) private mapViewEl: ElementRef;
-  view: any;
-  _map: any;
-  _params: any;
-  _qTask: any;
+  @ViewChild('mapViewNode', { static: true }) private mapViewEl: ElementRef;
+  dataSource: MatTableDataSource<Element>;
+ view: any;
+ map: any;
+  layerId: string;
+  params: any;
+  qTask: any;
   ELEMENT_DATA: any[] = [];
   FieldData: any[] = [];
-  AttrData: any;
-  highlightSelect: any;
+  pointObjectId: any;
+  graphicArray: any[] = [];
+  pointObjId: any[] = [];
+  lat: any;
+  log: any;
+  @Input() objectID: any[];
+  @Input() objData: any[];
   attributeName = [];
-  constructor(public sharedService: SharedService) {
+   constructor(public sharedService: SharedService) {
 
   }
 
   async initializeMap() {
-    let jsonArrayObject = [];
-    let attributeName = [];
+
     try {
-      // Load the modules for the ArcGIS API for JavaScript
-      const [Map, MapView, FeatureLayer] =
-        await loadModules(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/tasks/QueryTask",
-          "esri/tasks/support/Query"]);
+       const [Map, MapView,  QueryTask, Query, Graphic, GraphicsLayer] =
+        await loadModules(['esri/Map', 'esri/views/MapView',  'esri/tasks/QueryTask',
+          'esri/tasks/support/Query', 'esri/Graphic', 'esri/layers/GraphicsLayer']);
 
-      // Configure the Map
-      const mapProperties = {
-        basemap: "topo"
+       const mapProperties = {
+        basemap: 'topo'
       };
 
-      this._map = new Map(mapProperties);
-
-      // Initialize the MapView
-      const mapViewProperties = {
-        container: this.mapViewEl.nativeElement,
-        center: [-82.44384, 35.61318],
-        zoom: 8,
-        map: this._map
+       this.map = new Map(mapProperties);
+       const mapViewProperties = {
+      container: this.mapViewEl.nativeElement,
+      map: this.map
       };
 
-      this.view = new MapView(mapViewProperties);
+       this.view = new MapView(mapViewProperties);
+       const queryTask = {
+        url: 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/HUD%20REO%20Properties/FeatureServer/0'
+      };
 
-
-      const featureLayer = new FeatureLayer({
-        url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/HUD%20REO%20Properties/FeatureServer/0",
-        outFields: ['*'],
-        id: 'properties'
+       this.qTask = new QueryTask(queryTask);
+       const QueryFeature = {
+        returnGeometry: true,
+        outFields: ['*']
+      };
+       this.params = new Query(QueryFeature);
+       const qTask = new QueryTask({
+        url: 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/HUD%20REO%20Properties/FeatureServer/0'
       });
-      this.newMethod(featureLayer);
-      this.view.whenLayerView(featureLayer).then(layerView => {
-        console.log(layerView);
-        layerView.watch("updating", val => {
-          if (!val) {  // wait for the layer view to finish updating
-            layerView.queryFeatures({
-              outFields: layerView.availableFields
-            }).then(results => {
-              console.log(results)
-              this.getResults(results);
-            });
-          }
-        });
+       const params = new Query({
+        returnGeometry: true,
+        outFields: ['*']
       });
+       params.where = '1=1';
+       const viewNew = this.view;
+       qTask.execute(params).then((response) => {
+        this.getResults(response);
+        for ( const FieldDataValue of FieldData) {
+          const objId = FieldDataValue.OBJECTID;
+          const lat = FieldDataValue.MAP_LATITUDE;
+          const log = FieldDataValue.MAP_LONGITUDE;
+          const markerSymbol = {
+           type: 'simple-marker',
+           color: [226, 119, 40],
+           outline: {
+             color: [255, 255, 255],
+             width: 2
+           }
+         };
+          const point = {
+          type: 'point',
+          longitude: lat,
+          latitude: log
+        };
+          const attributesData = {
+              id :  objId
+        };
+          let visibleStatus = true;
+          if ( this.pointObjId.indexOf(objId) === -1) {
+              visibleStatus = false;       }
+          const graphic = new Graphic({
+            visible: visibleStatus,
+             geometry : point,
+             symbol : markerSymbol,
+             attributes : attributesData
 
-    } catch (error) {
-      console.log("EsriLoader: ", error);
-    }
-    this.view.when(function () {
-      var stationLayer = this.featureLayer.layers.getItemAt(1);
-      this.view.whenLayerView(stationLayer).then(function (layerView) {
-        var queryStations = stationLayer.createQuery();
-        stationLayer.queryFeatures(queryStations).then(function (result) {
-          // if a feature is already highlighted, then remove the highlight
-          if (highlightSelect) {
-            highlightSelect.remove();
+          });
+          graphicArray.push(graphic);
+          const layer = new GraphicsLayer({
+            visible: true,
+           graphics: graphicArray,
+            id : 'properties'
+         });
+          this.view.map.add(layer);
+          graphicArray.length = 0;
           }
+         })
+        .catch(this.promiseRejected);
+       const sharedServiceNew = this.sharedService;
+       viewNew.on('click', (event) => {
+        this.pointClick(event, viewNew, sharedServiceNew);
 
-          // the feature to be highlighted
-          var feature = result.features[0];
-
-          // use the objectID to highlight the feature
-          highlightSelect = layerView.highlight(
-            feature.attributes["OBJECTID"]
-          );
-        }
-    })
-    }//End Map
-
-  getResults(response) {
-        this.ELEMENT_DATA = [];
-        for(var i = 0; i<response.fields.length; i++) {
-      this.ELEMENT_DATA.push(response.fields[i].name)
-      ELEMENT_DATA.push(response.fields[i].name);
+         });
+          } catch (error) {
+      console.log('EsriLoader: ', error);
     }
-    this.FieldData = [];
-    response.features.forEach(value => {
-      this.FieldData.push(value.attributes);
+   }
+ getResults = (response: any) => {
+   for (const responseValue of response ) {
+      ELEMENT_DATA.push(response.fields.name);
+       }
+   response.features.forEach(value => {
+
       FieldData.push(value.attributes);
-    })
-    this.sharedService.ELEMENT_DATA.next(this.ELEMENT_DATA);
-    this.sharedService.FieldData.next(this.FieldData);
+      });
 
-    return { ELEMENT_DATA: ELEMENT_DATA, FieldData: FieldData };
-  }
-  filterData() {
-    const propertyLayer = this.view.map.layers.find((layer) => { return layer.id === 'properties' })
-
-    const str = (<HTMLInputElement>document.getElementById("textValue")).value;
-
-    if (str === '') {
-      propertyLayer.definitionExpression = null;
-    } else {
-      propertyLayer.definitionExpression = `ADDRESS like '%` + str + `%'`;
+   return { ELEMENT_DATA, FieldData};
     }
+    promiseRejected = (error) => {
+      console.error('Promise rejected: ', error.message);
 
-  }
-  closeData() {
-    /* const propertyLayer = this.view.map.layers.find((layer) => { return layer.id === 'properties' })
-     const str = (<HTMLInputElement>document.getElementById("textValue")).value;
-     if (str === '') {
-       propertyLayer.definitionExpression = null;
-     }*/
-  }
+    }
+    pointClick(event, viewNew , sharedServiceNew) {
+      let highlight: any;
+      const screenPoint = {
+      x : event.x,
+      y : event.y
+       };
+      viewNew.hitTest(screenPoint).then((response) => {
+       if (response) {
+         const graphicLayer = response.results[0].graphic.layer;
+         const obId = response.results[0].graphic.attributes.id;
+         viewNew.whenLayerView(graphicLayer).then((layerView) => {
+          if (highlight) {
+            highlight.remove();
+          }
+          highlight = layerView.highlight(response.results[0].graphic);
+        });
+         sharedServiceNew.pointObjectId.next(obId);
+       }
+       });
 
+     }
 
-  private newMethod(featureLayer: any) {
-    this._map.add(featureLayer);
-  }
-
-  ngOnInit() {
+    ngOnInit() {
 
     this.initializeMap();
     this.ELEMENT_DATA = ELEMENT_DATA;
-    console.log(this.ELEMENT_DATA);
     this.FieldData = FieldData;
+    this.sharedService.objData.subscribe(elementData => {
+      this.objData = elementData;
+      this.pointObjId.length = 0;
+      for (const objDataValue of elementData) {
+         this.pointObjId.push(objDataValue.OBJECTID);
+      }
+      const pointArr = this.pointObjId;
+      const propertyLayer = this.view.map.layers.find((layer) => layer.id === 'properties');
+      this.view.whenLayerView(propertyLayer).then((layerView) => {
+        const graphiclayer = layerView.layer;
+        if (graphiclayer.graphics.length > 0) {
+          const tempFeatures = graphiclayer.graphics.items;
 
-  }
+          for (const tempFeaturesValue of tempFeatures) {
+            const objectIDs = tempFeatures.attributes.id;
+            const result  = pointArr.indexOf(objectIDs);
+            if ( result === -1) {
+            tempFeatures.visible = false;
+            } else {
+              tempFeatures.visible = true;
+
+            }
+          }
+        }
+      });
+     }, error => {
+       console.log(error);
+     });
+    }
   ngOnDestroy() {
     if (this.view) {
-      // destroy the map view
       this.view.container = null;
     }
   }
