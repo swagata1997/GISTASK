e53118c3321119bef16189f8e56a076f39dbd048
swagata1997 <63637761+swagata1997@users.noreply.github.com>
2020-05-18 14:35:52 +0530

    18/05/20

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
 
   serviceData: any;
-  ELEMENT_DATA = new Subject<Array<any>>();
-  FieldData = new Subject<Array<any>>();
+  headers = new Subject<Array<any>>();
+  fieldData = new Subject<Array<any>>();
   objectID = new Subject<Array<any>>();
+  pointObjectId = new Subject<Array<any>>();
   objData = new Subject<Array<any>>();
   pointData = new Subject<Array<any>>();
   SelectedRow = '9415';
