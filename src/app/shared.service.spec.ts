0b08240f2cb1c7508fa60a984d35fefede82d14c
swagata1997 <63637761+swagata1997@users.noreply.github.com>
2020-05-26 20:01:52 +0530

    26/05/20

--- src/app/shared.service.spec.ts
+++ src/app/shared.service.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material';
+import { MatSortModule } from '@angular/material';
 import { SharedService } from './shared.service';
 
 describe('SharedService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule, HttpClientModule, MatTableModule, MatPaginatorModule, MatSortModule],
+        providers: [SharedService],
+        schemas: [NO_ERRORS_SCHEMA],
+  }));
 
   it('should be created', () => {
     const service: SharedService = TestBed.get(SharedService);
     expect(service).toBeTruthy();
   });
-});
+  afterAll(() => {
+    TestBed.resetTestingModule();
+  });
+ });
