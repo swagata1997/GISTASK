3ce830bae61b15e981567d17e06d4f7a4205f9eb
swagata1997 <63637761+swagata1997@users.noreply.github.com>
2020-05-26 20:05:08 +0530

    26/05/20

--- src/app/table/table.component.spec.ts
+++ src/app/table/table.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TableComponent } from './table.component';
 import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material';
 import { SharedService } from '../shared.service';
 import {HttpClientModule} from '@angular/common/http';
 describe('TableComponent', () => {
@@ -12,7 +13,7 @@ describe('TableComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ TableComponent ],
       schemas: [NO_ERRORS_SCHEMA],
-      imports: [MatTableModule, HttpClientModule],
+      imports: [ MatTableModule, MatInputModule, HttpClientModule],
       providers: [SharedService]
      })
     .compileComponents();
