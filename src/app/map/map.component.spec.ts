6fba8271a4de4d7d7fdfa24640e55dc6eaa06fe1
swagata1997 <63637761+swagata1997@users.noreply.github.com>
2020-06-01 22:05:47 +0530

    01/06/20

--- src/app/map/map.component.spec.ts
+++ src/app/map/map.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { MapComponent } from './map.component';
 import { SharedService } from '../shared.service';
@@ -38,25 +38,72 @@ describe('MapComponent', () => {
   it('should get response from getResults method', async () => {
     const [ Graphic] =  await loadModules(['esri/layers/GraphicsLayer', 'esri/Graphic']);
     const graphicArray = [];
-    const graphic = new Graphic();
+    const point = {
+      type: 'point',
+      longitude: -49.97,
+      latitude: 41.73
+    };
+    const markerSymbol = {
+      type: 'simple-marker',
+      color: [226, 119, 40],
+      outline: {
+        color: [255, 255, 255],
+        width: 2
+      }
+    };
+    const attributesData = {
+      id: component.objId
+    };
+    const graphic = new Graphic({
+      geometry : point,
+      symbol : markerSymbol,
+      attributes: attributesData
+    });
     graphicArray.push(graphic);
-    const graphicObj = {features: graphicArray, fields: [{name: 'OBJECTID'}, {name: 'CASE_NUM'}] };
+    const graphicObj = {features: graphicArray, fields: [{name: 'OBJECTID'}] };
     component.getResults(graphicObj);
-  
-    // expect(component.headers).toEqual('OBJECTID');
-     /* graphicObj.features.forEach(value => {
-    // fieldData.push(value.attributes);
-     expect(component.fieldData).toBe(value.attributes);
-    });*/
-   });
-
+    for (let i = 0; i <= graphicObj.fields.length; i++) {
+    expect( graphicObj.fields[i].name).toBe('OBJECTID');
+    }
+    for (let i = 0; i <= graphicObj.features.length; i++) {
+      expect(graphicObj.features[i].objId).toBe(1);
+     }
+     });
   it('should call graphicResult', async () => {
     const [ Graphic, GraphicsLayer] =  await loadModules([ 'esri/Graphic', 'esri/layers/GraphicsLayer']);
     const graphicArray = [];
-    const graphic = new Graphic();
-    graphicArray.push(graphic);
+    // const graphic = new Graphic();
+   // graphicArray.push(graphic);
     component.graphicResult();
+    expect();
      });
+  it('should call pointClick()' , async () => {
+    const [ Graphic] =  await loadModules([ 'esri/Graphic']);
+    const resultArray = [];
+    const  viewNew = component.view;
+   
+    spyOn(viewNew, 'on').and.callFake(() => {
+      component.pointClick(event, viewNew, sharedServiceNew);
+      expect(component.pointClick(event, viewNew, sharedServiceNew)).toHaveBeenCalled();
+    });
+    spyOn(viewNew, 'hitTest').and.callFake(() => {
+      });
+    expect(viewNew.hitTest()).toHaveBeenCalled();
+    const point = {
+      type: 'point',
+      longitude: -49.97,
+      latitude: 41.73
+    };
+    const attributesData = {
+      id: component.objId
+    };
+
+    const graphic = new Graphic({
+      geometry : point,
+      attributes : attributesData
+    });
+
+  });
   it('ngOnit call another function', () => {
     component.ngOnInit();
   });
